76c0cdf251f681ef30a2e1897b315cacb4867cc7
Rajan Kumar <rk4009023@gmail.com>
2025-07-26 12:48:20 +0530

    fix: match file name case for Linux deployment

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -7,7 +7,7 @@ import {
   register,
 } from "../controller/user.controller.js";
 
-import { isAuthenticated } from "../middleware/authUser.js";
+import { isAuthenticated } from "../middleware/authuser.js";
 
 const router = express.Router();
 

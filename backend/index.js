36823f259282cef8efb9027b80d86dd26f44e8b5
Rajan Kumar <rk4009023@gmail.com>
2026-03-23 00:24:49 +0530

    change production level

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: [
-    process.env.CLIENT_URL, // Keep for local development
+    process.env.FRONTEND_URL, // Keep for local development
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
